adb322e885d8d4c6e38ad8432371d99399af79c9
temporary not available <10041465+aaaaagold@users.noreply.github.com>
2019-01-25 19:11:04 +0800

    Create chrome-tmp-usr.bat

---
+++ chrome-tmp-usr.bat
@@ -0,0 +1,37 @@
+
+@rem without quotes:
+@set confDirDstPrefix=%TEMP%\chromeUsrData\User Data
+@set chomeExe=C:\bin\chrome\chrome.exe
+
+@set confDirSrc="%~fd0\..\chromeUsrDataTemplate"
+@set i=0
+:checkUsableFolderName_UseIt
+@set /A i+=1
+@echo %i%
+@set confDirDst="%confDirDstPrefix% %i%"
+
+@mkdir %confDirDst% ^
+ && ( title chrome - %USERNAME% - %i%
+ echo confDisDst=%confDirDst%
+ mkdir %confDirDst%\"SwReporter\VERSION\software_reporter_tool.exe"
+ xcopy /e /q %confDirSrc% %confDirDst%\ && "%chomeExe%" --incognito --user-data-dir=%confDirDst% 
+ goto mainProcessNotFound_check_lockfile 
+ exit /b 1 )
+@goto checkUsableFolderName_UseIt
+@goto END
+
+:mainProcessNotFound_check_lockfile
+@ping -n 2 localhost > nul 
+@if exist %confDirDst%\lockfile (
+ goto mainProcessNotFound_check_lockfile 
+)
+@ping -n 2 localhost > nul 
+@if exist %confDirDst%\lockfile (
+ goto mainProcessNotFound_check_lockfile 
+)
+@rmdir /s /q %confDirDst% 
+@echo directory: %confDirDst% removed
+@goto END
+
+:END
+@exit /b 0
